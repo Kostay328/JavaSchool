26859e74a90a3646b7f2f7bbec3431baa1fe8e3c
konnik konnik <konnik331@gmail.com>
2022-02-16 00:20:14 +0300

    5556

--- src/ru/progwards/java1/lessons/wrappers/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/wrappers/AccuracyDoubleFloat.java
@@ -2,12 +2,17 @@ package ru.progwards.java1.lessons.wrappers;
 
 public class AccuracyDoubleFloat {
     public static Double volumeBallDouble(Double radius){
-        return (4/3)*3.14*(radius*radius*radius);
+        return (4*3.14*radius*radius*radius)/3;
     }
     public static Float volumeBallFloat(Float radius){
-        return 4F/(3F*3.14F)*radius*radius*radius;
+        return (4F*3.14F*radius*radius*radius)/3F;
     }
     public static Double calculateAccuracy(Double radius){
         return volumeBallDouble(radius)-volumeBallFloat(Float.valueOf(Double.toString(radius))).doubleValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(volumeBallDouble(6371.2));
+        System.out.println(volumeBallFloat(6371.2F));
     }
 }
