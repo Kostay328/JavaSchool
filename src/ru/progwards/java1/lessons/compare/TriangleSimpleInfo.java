a5162f64ff3a8e8bc4fc74373491516d45ba5f04
konnik konnik <konnik331@gmail.com>
2022-02-19 23:38:33 +0300

    55567890

--- src/ru/progwards/java1/lessons/compare/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare/TriangleSimpleInfo.java
@@ -2,21 +2,21 @@ package ru.progwards.java1.lessons.compare;
 
 public class TriangleSimpleInfo {
     public static int maxSide(int a, int b, int c){
-        if(a < b && a < c)
-            return a;
-        else if(b < a && b < c)
-            return b;
-        else if(c < a && c < b)
-            return c;
-        else
-            return a;
-    }
-    public static int minSide(int a, int b, int c){
         if(a > b && a > c)
             return a;
         else if(b > a && b > c)
             return b;
         else if(c > a && c > b)
+            return c;
+        else
+            return a;
+    }
+    public static int minSide(int a, int b, int c){
+        if(a < b && a < c)
+            return a;
+        else if(b < a && b < c)
+            return b;
+        else if(c < a && c < b)
             return c;
         else
             return a;
