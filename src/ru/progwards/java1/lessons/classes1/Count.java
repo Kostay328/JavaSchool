be8e0b07f7a44973776b29e2cfcaa7508a2df70d
konnik konnik <konnik331@gmail.com>
2022-02-23 21:28:22 +0300

    1

--- src/ru/progwards/java1/lessons/classes1/Count.java
+++ src/ru/progwards/java1/lessons/classes1/Count.java
@@ -1,21 +1,29 @@
 package ru.progwards.java1.lessons.classes1;
 
 public class Count {
-    int count;
+    static int count;
     public Count(int count){
         this.count = count;
     }
 
-    public void inc(){
+    public static void inc(){
         count++;
     }
 
-    public int getCount(){
+    public static int getCount(){
         return count;
     }
 
-    public boolean dec(){
-        count++;
+    public static boolean dec(){
+        count = count - 1;
         return count <= 0;
+    }
+
+    public static void main(String[] args){
+        Count cnt = new Count(10);
+
+        while(!cnt.dec()){
+        }
+        System.out.println("count равен " + cnt.count);
     }
 }
