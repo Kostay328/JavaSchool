4b5740768c05bc734c808ac38e7e9ef47cc925db
konnik konnik <konnik331@gmail.com>
2022-03-01 23:30:52 +0300

    1

---
+++ src/ru/progwards/java1/lessons/useclasses/Count.java
@@ -0,0 +1,34 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class Count {
+    public Count(int count) {
+        this.count = count;
+    }
+    public Count() {
+        count = 0;
+    }
+
+
+    int count;
+
+    public void inc(){
+        count++;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public boolean dec(){
+        count = count - 1;
+        return count <= 0;
+    }
+
+    public static void main(String[] args){
+        Count cnt = new Count(10);
+
+        while(!cnt.dec()){
+        }
+        System.out.println("count равен " + cnt.count);
+    }
+}
---
+++ src/ru/progwards/java1/lessons/useclasses/CountTest.java
@@ -0,0 +1,29 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class CountTest {
+    public static void testInc(int count){
+        Count cnt = new Count();
+        for(int i = 0; i < count; i++){
+            cnt.inc();
+            System.out.print(cnt.getCount() + " ");
+        }
+        System.out.println("тест inc окончен");
+    }
+
+    public static void testDec(int count){
+        Count cnt = new Count(count);
+        while (!cnt.dec()){
+        }
+        System.out.println("count равен 0");
+    }
+
+    public static void main(String[] args) {
+        testInc(7);
+        testInc(0);
+        testInc(-1);
+        testDec(9);
+        testDec(0);
+        testDec(-5);
+
+    }
+}
