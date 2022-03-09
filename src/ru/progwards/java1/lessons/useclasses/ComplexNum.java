416ffc628f54a0e54e521832f158621ae15219a9
konnik konnik <konnik331@gmail.com>
2022-03-09 19:52:53 +0300

    1

--- src/ru/progwards/java1/lessons/useclasses/ComplexNum.java
+++ src/ru/progwards/java1/lessons/useclasses/ComplexNum.java
@@ -26,9 +26,15 @@ public class ComplexNum {
         int b1 = b * num.b + a * num.a;
         return new ComplexNum(a1, b1);
     }
+
+    public static void main(String[] args) {
+        ComplexNum n1 = new ComplexNum(1000, 1000);
+        ComplexNum n2 = new ComplexNum(100, 100);
+        System.out.println(n1.div(n2));
+    }
     public ComplexNum div(ComplexNum num) {
         int a1 = a / num.a;
-        int b1 = b / num.b;
+        int b1 = 0;
         return new ComplexNum(a1, b1);
     }
 }
