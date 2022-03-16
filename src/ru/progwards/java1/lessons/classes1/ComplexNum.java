804ac833d58dbd9505f95abc3741514b31107787
Kostya <kkorneev331@gmail.com>
2022-03-16 17:23:59 +0300

    1

--- src/ru/progwards/java1/lessons/classes1/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes1/ComplexNum.java
@@ -21,14 +21,22 @@ public class ComplexNum {
         int b1 = b - num.b;
         return new ComplexNum(a1, b1);
     }
-    public ComplexNum mul(ComplexNum num) {
-        int a1 = 0;
-        int b1 = b * num.b + a * num.a;
-        return new ComplexNum(a1, b1);
+    //умножение комплексных чисел по формуле:
+    //(a + bi) * (c + di) = (ac - bd) + (bc + ad)i
+    public ComplexNum mul(ComplexNum num){
+        return new ComplexNum(a * num.a - b * num.b, a * num.b + b * num.a);
     }
-    public ComplexNum div(ComplexNum num) {
-        int a1 = a / num.a;
-        int b1 = 0;
-        return new ComplexNum(a1, b1);
+    //деление комплексных чисел по формуле:
+    //(a + bi) / (c + di) = (ac + bd)/(cc+dd) + ((bc - ad)/(cc+dd))i
+    public ComplexNum div(ComplexNum num){
+        int t = num.a * num.a + num.b * num.b;
+        if (t == 0)
+            return new ComplexNum(0, 0);
+
+        int x = a * num.a + b * num.b;
+        int y = b * num.a - a * num.b;
+
+        return new ComplexNum(x / t, y / t);
     }
+    //Пытался сам разобраться в пердставленной формуле но так и не смог, спасибо за разьяснения
 }
--- src/ru/progwards/java1/lessons/useclasses/ComplexNum.java
+++ src/ru/progwards/java1/lessons/useclasses/ComplexNum.java
@@ -21,20 +21,22 @@ public class ComplexNum {
         int b1 = b - num.b;
         return new ComplexNum(a1, b1);
     }
-    public ComplexNum mul(ComplexNum num) {
-        int a1 = 0;
-        int b1 = b * num.b + a * num.a;
-        return new ComplexNum(a1, b1);
+    //умножение комплексных чисел по формуле:
+    //(a + bi) * (c + di) = (ac - bd) + (bc + ad)i
+    public ComplexNum mul(ComplexNum num){
+        return new ComplexNum(a * num.a - b * num.b, a * num.b + b * num.a);
     }
+    //деление комплексных чисел по формуле:
+    //(a + bi) / (c + di) = (ac + bd)/(cc+dd) + ((bc - ad)/(cc+dd))i
+    public ComplexNum div(ComplexNum num){
+        int t = num.a * num.a + num.b * num.b;
+        if (t == 0)
+            return new ComplexNum(0, 0);
 
-    public static void main(String[] args) {
-        ComplexNum n1 = new ComplexNum(1000, 1000);
-        ComplexNum n2 = new ComplexNum(100, 100);
-        System.out.println(n1.div(n2));
+        int x = a * num.a + b * num.b;
+        int y = b * num.a - a * num.b;
+
+        return new ComplexNum(x / t, y / t);
     }
-    public ComplexNum div(ComplexNum num) {
-        int a1 = a / num.a;
-        int b1 = 0;
-        return new ComplexNum(a1, b1);
-    }
+    //Пытался сам разобраться в пердставленной формуле но так и не смог, спасибо за разьяснения
 }
