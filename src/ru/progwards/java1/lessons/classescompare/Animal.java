749415e00c92940858913c33126f6c45f2b8bffe
Kostya <kkorneev331@gmail.com>
2022-03-17 19:47:53 +0300

    1

--- src/ru/progwards/java1/lessons/classescompare/Animal.java
+++ src/ru/progwards/java1/lessons/classescompare/Animal.java
@@ -33,13 +33,15 @@ abstract public class Animal {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof Animal animal))
-            return false;
-
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+        Animal animal = (Animal) o;
         return Double.compare(animal.getWeight(), getWeight()) == 0 &&
-                name.equals(animal.name) && animal.kind().equals(kind());
+                name.equals(animal.name) && animal.kind().equals(kind());    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, getWeight());
     }
 
     public Animal(String name){
