9ac5d502e4f55efdd42cdd6bcd3250319d2fb427
Kostya <kkorneev331@gmail.com>
2022-03-22 00:39:47 +0300

    1

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -17,7 +17,37 @@ public class DIntArray {
         dir.add(-88);
         dir.add(-8);
         dir.add(60);
+        dir.add(99);
+        dir.add(68);
+//        System.out.println(dir.at(0));
+//        dir.atInsert(4, -11);
         System.out.println(dir.at(0));
+        System.out.println(dir.at(1));
+        System.out.println(dir.at(2));
+        System.out.println(dir.at(3));
+        System.out.println(dir.at(4));
+        System.out.println(dir.at(5));
+        System.out.println(dir.at(6));
+        System.out.println(dir.at(7));
+        System.out.println(dir.at(8));
+        System.out.println(dir.at(9));
+        System.out.println(dir.at(10));
+        System.out.println(dir.at(11));
+        dir.atDelete(8);
+        System.out.println();
+        System.out.println(dir.at(0));
+        System.out.println(dir.at(1));
+        System.out.println(dir.at(2));
+        System.out.println(dir.at(3));
+        System.out.println(dir.at(4));
+        System.out.println(dir.at(5));
+        System.out.println(dir.at(6));
+        System.out.println(dir.at(7));
+        System.out.println(dir.at(8));
+        System.out.println(dir.at(9));
+        System.out.println(dir.at(10));
+//        System.out.println(dir.at(11));
+
     }
 
     public DIntArray(int[] array) {
@@ -31,16 +61,16 @@ public class DIntArray {
         array = tmp;
     }
     public void atInsert(int pos, int num){
-        int[] tmp = new int[array.length + 1];
+        int[] tmp = new int[array.length];
         System.arraycopy(array,0,tmp,0,pos);
-        tmp[pos-1] = num;
-        System.arraycopy(array, pos, tmp, pos,array.length-pos);
+        tmp[pos] = num;
+        System.arraycopy(array, pos, tmp, pos+1,(array.length-pos)-1);
         array = tmp;
     }
     public void atDelete(int pos){
         int[] tmp = new int[array.length - 1];
-        System.arraycopy(array,0,tmp,0,pos);
-        System.arraycopy(array, pos, tmp, pos,array.length-(pos+1));
+        System.arraycopy(array,0,tmp,0,pos+1);
+        System.arraycopy(array, pos+1, tmp, pos,(array.length-pos)-1);
         array = tmp;
     }
     public int at(int pos){
