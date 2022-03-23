5c23ac0aa6077c1685047bde53e3fb0c181079b4
konnik konnik <konnik331@gmail.com>
2022-03-24 01:57:11 +0300

    1

--- src/ru/progwards/java1/lessons/interfaces2/CompareHome.java
+++ src/ru/progwards/java1/lessons/interfaces2/CompareHome.java
@@ -7,7 +7,7 @@ public class CompareHome {
 
     public static void main(String[] args) {
         System.out.println("Результат сравнения Cow и Duck равен " + sameHome(new Cow("123"), new Duck("123")));
-        System.out.println("Результат сравнения Cow и Hamster " + sameHome(new Cow("123"), new Hamster("123")));
+        System.out.println("Результат сравнения Cow и Hamster равен " + sameHome(new Cow("123"), new Hamster("123")));
         System.out.println("Результат сравнения Cow и Hawk равен " + sameHome(new Cow("123"), new Hawk()));
         System.out.println("Результат сравнения Duck и Hamster равен " + sameHome(new Duck("123"), new Hamster("123")));
         System.out.println("Результат сравнения Duck и Hawk равен " + sameHome(new Duck("123"), new Hawk()));
