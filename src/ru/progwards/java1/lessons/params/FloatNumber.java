0cacf373114ece78ffa83cccc068134f76933e3c
konnik konnik <konnik331@gmail.com>
2022-03-27 00:31:01 +0300

    1

---
+++ src/ru/progwards/java1/lessons/params/FloatNumber.java
@@ -0,0 +1,55 @@
+package ru.progwards.java1.lessons.params;
+
+public class FloatNumber {
+    boolean sign;
+    long mantissa;
+    int exp;
+
+    public FloatNumber(boolean sign, long mantissa, int exp) {
+        this.sign = sign;
+        this.mantissa = mantissa;
+        this.exp = exp;
+    }
+
+    FloatNumber(String num){
+        num = num.trim().replace(",", "").replace(".", "");
+        char[] cl = num.toCharArray();
+        if(cl[0] == '-')
+            sign = false;
+        else
+            sign = true;
+        String r = "";
+        String r0 = "";
+        for (int i = sign ? 0 : 1; i < cl.length; i++){
+            if(Character.isDigit(cl[i]))
+                r += cl[i];
+            else {
+                for (int j = i+1; j < cl.length; j++){
+                    r0 += cl[j];
+                }
+                break;
+            }
+        }
+        mantissa = Long.getLong(r);
+        exp = Integer.getInteger(r0);
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        if(!sign)
+            res += "-";
+        char[] cl = Long.toString(mantissa).toCharArray();
+        res += cl[0] + ".";
+        for (int i = 2; i < cl.length; i++){
+            res += cl[i];
+        }
+        if(exp > 0) {
+            res += "E" + exp;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+    }
+}
