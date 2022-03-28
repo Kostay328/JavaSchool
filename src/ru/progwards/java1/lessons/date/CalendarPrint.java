053ac92ab5e891162cf9f44a619a19bfed7dae6b
konnik konnik <konnik331@gmail.com>
2022-03-29 02:44:56 +0300

    1

--- src/ru/progwards/java1/lessons/date/CalendarPrint.java
+++ src/ru/progwards/java1/lessons/date/CalendarPrint.java
@@ -1,9 +1,20 @@
 package ru.progwards.java1.lessons.date;
 
+import java.text.DateFormatSymbols;
 import java.time.LocalDate;
 
 public class CalendarPrint {
+    public static String getMonthForInt(int num) {
+        String month = "wrong";
+        DateFormatSymbols dfs = new DateFormatSymbols();
+        String[] months = dfs.getMonths();
+        if (num >= 0 && num <= 11 ) {
+            month = months[num];
+        }
+        return month.substring(0, 1).toUpperCase() + month.substring(1);
+    }
     public static void printMonth(int month, int year){
+        System.out.println(year + " " + getMonthForInt(month));
         System.out.println("пн вт ср чт пт сб вс");
         LocalDate now = LocalDate.of(year, month, 1);
         int nowMonth = now.getMonthValue();
