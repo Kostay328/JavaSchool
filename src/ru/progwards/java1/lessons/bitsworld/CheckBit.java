94d288baa2ce64f7f5ffc7d86ad7de2854513ab7
Kostya328 <kkorneev331@gmail.com>
2023-07-12 03:59:03 +0300

    2

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -5,14 +5,17 @@ public class CheckBit {
         byte b = 0b0000001;
         int f = value;
         int res = 0;
-        for (int i = 0; i<bitNumber; i++) {
-            res = (b & f) > 0 ? 1 : 0;
-            f>>=1;
+        for (int i = 8; i>bitNumber; i--) {
+            if(i == 8-bitNumber) {
+                res = (b & f) > 0 ? 1 : 0;
+                f >>= 1;
+            }
         }
         return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkBit((byte) 0b1110010, 3));
+        System.out.println(checkBit((byte) 0b11001101, 0));
+        System.out.println(checkBit((byte) 71, 0));
     }
 }
