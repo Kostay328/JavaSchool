d7d35315bad04d1a332d5f90d1911a5696b6b214
Kostya328 <kkorneev331@gmail.com>
2024-01-10 12:18:04 +0300

    2

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -2,10 +2,11 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
     public static int checkBit(byte value, int bitNumber) {
-        byte b = -127;
+        byte b = 1;
+//        System.out.println(new Binary(b).toString());
         int f = value;
         f >>= bitNumber;
-        System.out.println(f);
+//        System.out.println(new Binary((byte)f).toString());
         return (b & f) > 0 ? 1 : 0;
     }
 
@@ -13,8 +14,9 @@ public class CheckBit {
         System.out.println(checkBit((byte) 5, 0));//1
         System.out.println(checkBit((byte) 114, 3));//0
         System.out.println(checkBit((byte) -51, 1));//0
-        System.out.println(checkBit((byte) 71, 1));//0
+        System.out.println(checkBit((byte) 71, 0));//0
         System.out.println(checkBit((byte) -127, 7));//1
         System.out.println(checkBit((byte) 3, 0));//1
+        System.out.println(checkBit((byte) -66, 5));//1
     }
 }
