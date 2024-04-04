c091282e250b0e0fde2d88d7efb1d673b8d82083
Kostya328 <kkorneev331@gmail.com>
2024-04-05 01:04:00 +0300

    2

--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -43,10 +43,41 @@ public class StackCalc {
 
 class Calculate {
     public static double calculation1() {
-        return 0;
+        StackCalc calc1 = new StackCalc();
+
+        calc1.push(2.2);
+        calc1.push(3);
+        calc1.push(12.1);
+
+        calc1.add();
+        calc1.mul();
+
+        return calc1.pop();
     }
 
     public static double calculation2() {
-        return 0;
+        StackCalc calc2 = new StackCalc();
+
+        calc2.push(737.22);
+        calc2.push(24);
+        calc2.add();
+        calc2.push(55.6);
+        calc2.push(12.1);
+        calc2.sub();
+        calc2.div();
+        calc2.push(19);
+        calc2.push(3.33);
+        calc2.sub();
+        calc2.push(87);
+        calc2.push(2);
+        calc2.push(13.001);
+        calc2.push(9.2);
+        calc2.sub();
+        calc2.mul();
+        calc2.add();
+        calc2.mul();
+        calc2.add();
+
+        return calc2.pop();
     }
 }
