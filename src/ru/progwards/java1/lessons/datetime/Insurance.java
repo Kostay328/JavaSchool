243f9798b4c26b6035bdf9bbeb0da49f8050c824
Kostya328 <kkorneev331@gmail.com>
2024-04-18 01:15:51 +0300

    2

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -2,6 +2,13 @@ package ru.progwards.java1.lessons.datetime;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAccessor;
+import java.util.Locale;
 
 public class Insurance {
     public enum FormatStyle {SHORT, LONG, FULL}
@@ -11,7 +18,6 @@ public class Insurance {
 
     public Insurance(ZonedDateTime start) {
         this.start = start;
-        setDuration(Duration.ZERO);
     }
 
     public Insurance(String strStart, FormatStyle style) {
@@ -26,7 +32,7 @@ public class Insurance {
                 System.out.println(dtf);
                 this.start = ZonedDateTime.parse(strStart, dtf);
                 break;
-            default:
+            default: // case: FULL
                 dtf = DateTimeFormatter.ISO_ZONED_DATE_TIME;
                 this.start = ZonedDateTime.parse(strStart, dtf);
         }
@@ -37,25 +43,16 @@ public class Insurance {
         this.duration = duration;
     }
 
-    public void setDuration(ZonedDateTime expiration) {
-        this.duration = Duration.between(start, expiration);
-    }
-
-    public void setDuration(int months, int days, int hours) {
-        ZonedDateTime expiration = start.plusMonths(months).plusDays(days).plusHours(hours);
-        this.duration = Duration.between(start, expiration);
-    }
-
     public void setDuration(String strDuration, FormatStyle style) {
-        DateTimeFormatter dtf;
+        DateTimeFormatter formatter;
         switch (style) {
             case SHORT:
                 this.duration = Duration.ofMillis(Long.parseLong(strDuration));
                 break;
             case LONG:
-                dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.systemDefault());
-                LocalDateTime ldt = LocalDateTime.parse(strDuration, dtf);
-                LocalDateTime zero = LocalDateTime.of(0, 1, 1, 0, 0).minusMonths(1).minusDays(1);
+                formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.systemDefault());
+                LocalDateTime ldt = LocalDateTime.parse(strDuration, formatter);
+                LocalDateTime zero = LocalDateTime.MIN;
                 this.duration = Duration.between(zero, ldt);
                 break;
             default:
@@ -63,10 +60,20 @@ public class Insurance {
         }
     }
 
+    public void setDuration(ZonedDateTime expiration) {
+        this.duration = Duration.between(start, expiration);
+    }
+
+    public void setDuration(int months, int days, int hours) {
+        ZonedDateTime endDate = start.plusMonths(months).plusDays(days).plusHours(hours);
+        this.duration = Duration.between(start, endDate);
+    }
+
     public boolean checkValid(ZonedDateTime dateTime) {
-        if (duration.equals(Duration.ZERO) && dateTime.isAfter(start)) return true;
-        ZonedDateTime endTime = start.plusHours(duration.toHours());
-        return dateTime.isAfter(start) && dateTime.isBefore(endTime);
+        if (duration == null) {
+            return true;
+        }
+        return dateTime.isBefore(start.plus(duration));
     }
 
     public String toString() {
@@ -77,4 +84,16 @@ public class Insurance {
         return "Insurance issued on " + start + validStr;
     }
 
+    public static void main(String[] args) {
+        //TemporalAccessor: {},ISO,Europe/Moscow resolved to 2023-03-08 of type java.time.format.Parsed
+        TemporalAccessor ta = DateTimeFormatter.ISO_ZONED_DATE_TIME.parse("2023-03-12T18:06:11.940343+03:00[Europe/Moscow]");
+        ZonedDateTime test = ZonedDateTime.from(Instant.now().atZone(ZoneId.systemDefault()));
+        ZonedDateTime test2 = ZonedDateTime.of(2023, 3, 9, 1, 0, 0, 0, ZoneId.systemDefault());
+        Insurance pu = new Insurance("2023-03-14T08:36:12.899241+03:00[Europe/Moscow]", FormatStyle.FULL);
+        Insurance fromTA = new Insurance(ZonedDateTime.from(ta));
+        fromTA.setDuration(0, 0, 1);
+
+        System.out.println(fromTA);
+
+    }
 }
