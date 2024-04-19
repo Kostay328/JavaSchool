2153420a3f2ed6c2e2fe24d58e624820b399c05b
Kostya328 <kkorneev331@gmail.com>
2024-04-19 23:30:05 +0300

    2

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -28,7 +28,7 @@ public class SessionManager {
 
     public UserSession get(int sessionHandle) {
         UserSession session = sessionMap.get(sessionHandle);
-        if (session == null || Duration.between(session.getLastAccess(), LocalDateTime.now()).toSeconds() >= 0) {
+        if (session == null || Duration.between(session.getLastAccess(), LocalDateTime.now()).toSeconds() >= 1) {
             return null;
         }
         session.updateLastAccess();
