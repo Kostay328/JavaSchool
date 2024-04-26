339556001ddf5a0c0d284af6b1f6aa496d53835a
Kostya328 <kkorneev331@gmail.com>
2024-04-26 03:20:23 +0300

    2

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -1,45 +1,70 @@
 package ru.progwards.java1.lessons.files;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class FindDuplicates {
-    public List<List<String>> findDuplicates(String startPath) {
-        try {
-            List<List<String>> duplicates = new ArrayList<>();
-            Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    String filePath = file.toString();
-                    String fileName = filePath.substring(startPath.length() + 1); // Получаем имя файла
-                    String fileContent = new String(Files.readAllBytes(file));
+    public List<List<String>> findDuplicates(String startPath) throws IOException {
+        List<List<String>> res = new ArrayList<>();
+        List<FileData> fdl = new ArrayList<>();
 
-                    // Проверяем, есть ли уже дубликаты с таким же именем, размером, датой и содержимым
-                    for (List<String> duplicate : duplicates) {
-                        if (duplicate.get(0).equals(fileName) &&
-                                duplicate.get(1).equals(String.valueOf(attrs.size())) &&
-                                Objects.equals(fileContent, new String(Files.readAllBytes(Paths.get(duplicate.get(3)))))) {
-                            // Если дубликат найден, добавляем текущий путь к списку дубликатов
-                            duplicate.add(filePath);
-                            return FileVisitResult.CONTINUE;
-                        }
-                    }
-
-                    // Если дубликат не найден, создаем новый список для хранения информации о дубликате
-                    List<String> newDuplicate = new ArrayList<>();
-                    duplicates.add(newDuplicate);
-
-                    return FileVisitResult.CONTINUE;
+        Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                File fileStr = new File(file.toString());
+                if (fileStr.isFile()) {
+                    FileData fileData = new FileData();
+                    fileData.name = file.getFileName().toString();
+                    fileData.lastModified = fileStr.lastModified();
+                    fileData.length = fileStr.length();
+                    fileData.file = fileStr;
+                    fileData.path = file.toAbsolutePath();
+                    fileData.pathList.add(file.toAbsolutePath().toString());
+                    fdl.add(fileData);
                 }
-            });
-            return duplicates;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        for (int i = 0; i < fdl.size(); i++) {
+            for (int j = i + 1; j < fdl.size(); ) {
+                if (compareFileData(fdl.get(i), fdl.get(j))) {
+                    fdl.get(i).pathList.add(fdl.get(j).path.toString());
+                    fdl.remove(j);
+                    continue;
+                }
+                j++;
+            }
         }
+
+        for (FileData fd : fdl) {
+            if (fd.pathList.size() > 1)
+                res.add(fd.pathList);
+        }
+        return res;
     }
+
+    private boolean compareFileData(FileData fd1, FileData fd2) throws IOException {
+        if (!fd1.name.equals(fd2.name) || fd1.length != fd2.length || fd1.lastModified != fd2.lastModified)
+            return false;
+        return Arrays.equals(Files.readAllBytes(fd1.path), Files.readAllBytes(fd2.path));
+    }
+}
+class FileData {
+    String name;
+    long lastModified;
+    long length;
+    File file;
+    Path path;
+    List<String> pathList = new ArrayList<>();
 }
