e7fc1d2b2e065f90702b4bd50318e45ade0fa858
Kostya328 <kkorneev331@gmail.com>
2024-05-05 03:22:08 +0300

    2

---
+++ src/ru/progwards/java1/lessons/recursion/AsNumbersSum.java
@@ -0,0 +1,71 @@
+package ru.progwards.java1.lessons.recursion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class AsNumbersSum {
+    static List<List<Integer>> buffer = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println(asNumbersSum(5));
+    }
+
+    public static String asNumbersSum(int number) {
+        if (number == 0) {
+            return "0";
+        }
+
+        int[] cur = new int[number];
+        cur[0] = number;
+        generateCombinations(cur, buffer);
+
+        String resStr = number+"";
+        for (List<Integer> rl:buffer) {
+            boolean firstAdd = true;
+            resStr += " = ";
+            for (Integer i:rl) {
+                if(firstAdd){
+                    firstAdd = false;
+                    resStr += i;
+                }else
+                    resStr += "+" + i;
+            }
+        }
+        return resStr;
+    }
+
+    private static void generateCombinations(int[] current, List<List<Integer>> result) {
+        boolean all1 = true;
+        for (int c:current) {
+            if (c > 1) {
+                all1 = false;
+                break;
+            }
+        }
+
+        if (all1)
+            return;
+
+        for (int i = current.length-1; i >= 0; i=i-1) {
+            boolean needAdd = true;
+            List<Integer> nl = new ArrayList<>();
+            if(current[i]>1) {
+                current[i] = current[i]-1;
+                current[i+1] = current[i+1]+1;
+                for (int c:current) {
+                    if(c>0)
+                        nl.add(c);
+                }
+                for (int k = 0; nl.size() > k+1; k++) {
+                    if(nl.get(k) == 1 && nl.get(k+1) == 2)
+                        needAdd = false;
+                }
+                if(needAdd)
+                    result.add(nl);
+                break;
+            }
+        }
+            generateCombinations(current, result);
+    }
+}
---
+++ src/ru/progwards/java1/lessons/recursion/GoodsWithLambda.java
@@ -0,0 +1,77 @@
+package ru.progwards.java1.lessons.recursion;
+
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Goods {
+    String name;
+    String number;
+    int available;
+    double price;
+    Instant expired;
+
+    public Goods(String name, String number, int available, double price, Instant expired) {
+        this.name = name;
+        this.number = number;
+        this.available = available;
+        this.price = price;
+        this.expired = expired;
+    }
+}
+
+public class GoodsWithLambda {
+    List<Goods> goodsList;
+
+    public void setGoods(List<Goods> list) {
+        goodsList = list;
+    }
+
+    public List<Goods> sortByName() {
+        return goodsList.stream()
+                .sorted(Comparator.comparing(goods -> goods.name))
+                .collect(Collectors.toList());
+    }
+
+    public List<Goods> sortByNumber() {
+        return goodsList.stream()
+                .sorted(Comparator.comparing(goods -> goods.number.toUpperCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Goods> sortByPartNumber() {
+        return goodsList.stream()
+                .sorted(Comparator.comparing(goods -> goods.number.substring(0, 3).toUpperCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Goods> sortByAvailabilityAndNumber() {
+        return goodsList;
+    }
+
+    public List<Goods> expiredAfter(Instant date) {
+        return goodsList.stream()
+                .filter(goods -> goods.expired.isAfter(date))
+                .sorted(Comparator.comparing(goods -> goods.expired))
+                .collect(Collectors.toList());
+    }
+
+    public List<Goods> countLess(int count) {
+        return goodsList.stream()
+                .filter(goods -> goods.available < count)
+                .sorted(Comparator.comparing(goods -> goods.available))
+                .collect(Collectors.toList());
+    }
+
+    public List<Goods> countBetween(int count1, int count2) {
+        return goodsList.stream()
+                .filter(goods -> goods.available > count1 && goods.available < count2)
+                .sorted().collect(Collectors.toList());
+    }
+
+    public GoodsWithLambda(List<Goods> goods) {
+        goodsList = goods;
+    }
+}
+
