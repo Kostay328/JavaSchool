c5adbcd0374715a2020a1cf9109c6c4e7ff92500
Kostya328 <kkorneev331@gmail.com>
2024-05-19 02:39:33 +0300

    2

--- src/ru/progwards/java1/lessons/generics/FruitBox.java
+++ src/ru/progwards/java1/lessons/generics/FruitBox.java
@@ -2,50 +2,52 @@ package ru.progwards.java1.lessons.generics;
 
 import java.util.ArrayList;
 
-public class FruitBox extends ArrayList<Fruit> {
-    public boolean add(Fruit fruit) {
-        if (size() == 0 || get(0).getClass().equals(fruit.getClass())) {
-            super.add(fruit);
-            return true;
-        }else
-            return false;
+public class FruitBox<E extends Fruit> extends ArrayList<Fruit> implements Comparable<FruitBox> {
+
+    @Override
+    public boolean add(Fruit f) {
+        if (size() == 0 || get(0).getClass().equals(f.getClass()))
+            return super.add(f);
+        return false;
     }
 
-    public double getWeight() {
-        return stream().mapToDouble(Fruit::getWeight).sum();
+    public float getWeight() {
+        return size() > 0 ? size() * get(0).getWeight() : 0;
     }
 
-    public void moveTo(FruitBox otherBox) {
-        if (otherBox != null && getClass().equals(otherBox.getClass())) {
-            otherBox.clear();
-            otherBox.addAll(this);
-            clear();
-        } else {
+    void moveTo(FruitBox<Fruit> fb) throws UnsupportedOperationException {
+        try {
+            if (get(0).getClass().equals(fb.get(0).getClass())) {
+                fb.addAll(this);
+                clear();
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        } catch (Exception e) {
             throw new UnsupportedOperationException();
         }
+    }
+
+    @Override
+    public int compareTo(FruitBox fb) {
+        return Float.compare(getWeight(), fb.getWeight());
     }
 }
 
 abstract class Fruit {
-    protected double weight;
-
-    public Fruit(double weight) {
-        this.weight = weight;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
+    abstract float getWeight();
 }
 
 class Apple extends Fruit {
-    public Apple() {
-        super(1.0);
+    @Override
+    public float getWeight() {
+        return 1f;
     }
 }
 
 class Orange extends Fruit {
-    public Orange() {
-        super(1.5);
+    @Override
+    public float getWeight() {
+        return 1.5f;
     }
 }
